2c5f3dab37aa2385110e30e6637400c2e5b8bb7e
vipguy7 <124065582+vipguy7@users.noreply.github.com>
2025-05-17 11:01:16 +0630

    Update font-loader.ts

--- utils/font-loader.ts
+++ utils/font-loader.ts
@@ -1,19 +1,18 @@
 export const loadFont = (fontName: string, fontUrl: string): Promise<void> => {
   return new Promise((resolve, reject) => {
     if (document.fonts.check(`1em ${fontName}`)) {
-      return resolve()
+      return resolve();
     }
-
-    const font = new FontFace(fontName, `url(${fontUrl})`)
+    const font = new FontFace(fontName, `url(${fontUrl})`);
     font
       .load()
       .then((loadedFont) => {
-        document.fonts.add(loadedFont)
-        resolve()
+        document.fonts.add(loadedFont);
+        resolve();
       })
       .catch((error) => {
-        console.error(`Failed to load font: ${fontName}`, error)
-        reject(error)
-      })
-  })
-}
+        console.error(`Failed to load font: ${fontName}`, error);
+        reject(error);
+      });
+  });
+};
